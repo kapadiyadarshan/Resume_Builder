b42ffcefac801562afb0aa55c71f7f50e2101389
kapadiyadarshan <darshan.kapadia.35@gmail.com>
2023-07-14 11:04:31 +0530

    Education Page

--- resume_builder/lib/views/screens/education.dart
+++ resume_builder/lib/views/screens/education.dart
@@ -10,6 +10,9 @@ class Education extends StatefulWidget {
 }
 
 class _EducationState extends State<Education> {
+
+  int listEdu = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +23,185 @@ class _EducationState extends State<Education> {
         backgroundColor: theme1,
         foregroundColor: theme2,
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: SingleChildScrollView(
+          child: Column(
+            children: List.generate(listEdu, (index) =>
+                Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(12),
+
+                      decoration: BoxDecoration(
+                          color: theme2,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: const [
+                            BoxShadow(
+                                blurRadius: 10,
+                                color: Colors.grey,
+                                offset: Offset(5, 5)
+                            ),
+                          ]
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text("Education ${index+1}",
+                                style: TextStyle(
+                                    color: theme1,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold
+                                ),),
+                              const Spacer(),
+                              IconButton(
+                                  onPressed: () {
+                                    setState(() {
+
+                                    });
+                                  },
+                                  icon: Icon(Icons.delete,
+                                  color: theme1,
+                                  )
+                              )
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+
+                          // Couse / Degree
+                          TextFormField(
+                            maxLines: 1,
+                            cursorColor: theme1,
+                            decoration: InputDecoration(
+                                isDense: true,
+                                labelText: "Course / Degree",
+                                labelStyle: TextStyle(
+                                    color: theme1
+                                ),
+                                hintText: "Enter Your Course / Degree",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10)
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: theme1,
+                                    width: 2,
+                                  ),
+                                )
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+
+                          //School / University
+                          TextFormField(
+                            maxLines: 1,
+                            cursorColor: theme1,
+                            decoration: InputDecoration(
+                                isDense: true,
+                                labelText: "School / University",
+                                labelStyle: TextStyle(
+                                    color: theme1
+                                ),
+                                hintText: "Enter Your School / University",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10)
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: theme1,
+                                    width: 2,
+                                  ),
+                                )
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+
+                          //Grade / Score
+                          TextFormField(
+                            maxLines: 1,
+                            cursorColor: theme1,
+                            decoration: InputDecoration(
+                                isDense: true,
+                                labelText: "Grade / Score",
+                                labelStyle: TextStyle(
+                                    color: theme1
+                                ),
+                                hintText: "Enter Your Grade / Score",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10)
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: theme1,
+                                    width: 2,
+                                  ),
+                                )
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+
+                          //Year
+                          TextFormField(
+                            maxLines: 1,
+                            cursorColor: theme1,
+                            decoration: InputDecoration(
+                                isDense: true,
+                                labelText: "Year",
+                                labelStyle: TextStyle(
+                                    color: theme1
+                                ),
+                                hintText: "Enter Year",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10)
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: theme1,
+                                    width: 2,
+                                  ),
+                                )
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                  ],
+                ),
+            )
+          ),
+        ),
+      ),
+      floatingActionButton: ElevatedButton.icon(
+          onPressed: () {
+            setState(() {
+              listEdu++;
+            });
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: theme1,
+            foregroundColor: theme2,
+          ),
+          icon: Icon(Icons.add),
+          label: Text("Add")
+      ),
+      backgroundColor: Colors.grey.shade200,
     );
   }
 }
